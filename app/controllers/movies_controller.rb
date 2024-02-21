466c1d5c28915d6856d51aca791201dd90cd699e
Salvador Castaneda <57605337+salcasta@users.noreply.github.com>
2024-02-21 22:56:06 +0000

    walkthrough video complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,6 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
+
   def new
     @movie = Movie.new
   end
@@ -14,12 +16,11 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie = Movie.find(params.fetch(:id))
+    
   end
 
+
   def create
-    movie_params = params.require(:movie).permit(:title, :description)
-    
     @movie = Movie.new(movie_params)
 
     if @movie.valid?
@@ -32,14 +33,10 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    @movie = Movie.find(params.fetch(:id))
+    
   end
 
   def update
-    @movie = Movie.find(params.fetch(:id))
-
-    movie_params = params.require(:movie).permit(:title, :description)
-    
     if @movie.update(movie_params)
       redirect_to @movie, notice: "Movie updated successfully."
     else
@@ -48,10 +45,18 @@ class MoviesController < ApplicationController
   end
 
   def destroy
-    @movie = Movie.find(params.fetch(:id))
-
     @movie.destroy
 
     redirect_to movies_url, notice: "Movie deleted successfully."
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params.fetch(:id))
+  end
+
+  def movie_params
+    movie_params = params.require(:movie).permit(:title, :description)
   end
 end
